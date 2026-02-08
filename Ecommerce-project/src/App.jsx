9a0c94756cd5a8f08ca373f654f0715c996cacf2
OmSingh2007 <osingh3639@gmail.com>
2026-02-08 10:39:56 +0530

    TrackPackage and the orders pages is moved to react

--- Ecommerce-project/src/App.jsx
+++ Ecommerce-project/src/App.jsx
@@ -2,6 +2,8 @@ import { Routes } from 'react-router'
 import { Route } from 'react-router'
 import { HomePage } from './Pages/HomePage'
 import { CheckoutPage } from './Pages/CheckoutPage'
+import { OrdersPage } from './Pages/OrdersPage'
+import { TrackPackage } from './Pages/TrackPackage'
 import './App.css'
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage/>} />
+      <Route path="orders" element={<OrdersPage/>} />
+      <Route path="tracking" element={<TrackPackage/>} />
     </Routes> 
   )
 }
