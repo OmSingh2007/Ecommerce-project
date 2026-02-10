91fc17ee6c3edaa587b4777837e6e0410804a7fc
OmSingh2007 <osingh3639@gmail.com>
2026-02-10 16:19:00 +0530

    The useEffect is fixed

--- Ecommerce-project/src/App.jsx
+++ Ecommerce-project/src/App.jsx
@@ -11,11 +11,11 @@ import './App.css'
 function App() {
   const [cart , setCart] = useState([]);
   useEffect(()=>{
-    axios.get("/api/cart-items")
+    axios.get("/api/cart-items?expand=product")  // (?expand=product) is called a query parameter 
            .then((response)=>{
             setCart(response.data);
          });
-  });
+  },[]);
   
 
   return (
