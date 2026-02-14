ca1cf7032d4177ca0ac5ba8672280acbf9a78c35
OmSingh2007 <osingh3639@gmail.com>
2026-02-14 10:43:02 +0530

    Seperate into smaller components

---
+++ Ecommerce-project/src/Pages/orders/OrdersPage.jsx
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import { OrdersGrid } from './OrdersGrid'
+import { useState, useEffect, Fragment } from 'react'
+import { Header } from '../../components/Header'
+import './OrdersPage.css'
+export function OrdersPage({ cart }) {
+    const [orders, setOrders] = useState([]);
+    useEffect(() => {
+        axios.get('/api/orders?expand=products')
+            .then((response) => {
+                setOrders(response.data)
+            });
+    }, []);
+    return (
+        <>
+            <title>Orders</title>
+
+            <Header cart={cart} />
+            <div className="orders-page">
+                <div className="page-title">Your Orders</div>
+                <OrdersGrid orders={orders} />
+            </div>
+        </>
+    );    
+}
