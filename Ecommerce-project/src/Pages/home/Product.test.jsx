a130ebc5c11913447e356e5dc18f39f154a57206
OmSingh2007 <osingh3639@gmail.com>
2026-02-15 15:58:15 +0530

    The product variable is shift inside the it body

--- Ecommerce-project/src/Pages/home/Product.test.jsx
+++ Ecommerce-project/src/Pages/home/Product.test.jsx
@@ -1,27 +1,31 @@
-import { it , describe, expect , vi } from 'vitest'
-import { Product } from './Product' 
-import { render,screen } from '@testing-library/react'
+import { it, describe, expect, vi } from 'vitest'
+import { Product } from './Product'
+import { render, screen } from '@testing-library/react'
 
-describe('ProductComponent', ()=>{
-    const product={
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090,
-    keywords: ["socks", "sports", "apparel"]
-  };
-    const fetchCartData=vi.fn();
-    it('displays the product details correctly ',()=>{
+describe('ProductComponent', () => {
+    const fetchCartData = vi.fn();
+    it('displays the product details correctly ', () => {
+        const product = {
+            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+            rating: {
+                stars: 4.5,
+                count: 87
+            },
+            priceCents: 1090,
+            keywords: ["socks", "sports", "apparel"]
+        };
         render(<Product product={product} fetchCartData={fetchCartData} />)
-        
+
         expect(screen.getByText('Black and Gray Athletic Cotton Socks - 6 Pairs')).toBeInTheDocument();
         expect(screen.getByText('$10.90')).toBeInTheDocument();
-        expect(screen.getByTestId('product-image')).toHaveAttribute('src','images/products/athletic-cotton-socks-6-pairs.jpg');
-        expect(screen.getByTestId('product-rating-stars-image')).toHaveAttribute('src','images/ratings/rating-45.png');
+        expect(screen.getByTestId('product-image')).toHaveAttribute('src', 'images/products/athletic-cotton-socks-6-pairs.jpg');
+        expect(screen.getByTestId('product-rating-stars-image')).toHaveAttribute('src', 'images/ratings/rating-45.png');
         expect(screen.getByText('87')).toBeInTheDocument();
     });
+
+    it('adds a product to the cart ', () => {
+
+    })
 });
