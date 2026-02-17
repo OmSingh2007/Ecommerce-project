b036466142dbbe0c5f2c7b3601a80e9671f94718
OmSingh2007 <osingh3639@gmail.com>
2026-02-17 07:10:09 +0530

    Create a Integration test for home page

--- Ecommerce-project/src/Pages/home/Product.jsx
+++ Ecommerce-project/src/Pages/home/Product.jsx
@@ -17,7 +17,7 @@ export function Product({ product, fetchCartData}) {
                 };
 
     return (
-        <div key={product.id} className="product-container">
+        <div key={product.id} className="product-container" data-testid="product-container">
             <div className="product-image-container">
                 <img className="product-image"
                     data-testid="product-image"
